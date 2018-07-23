mumoshu/cilium

commit 6a8b48951470e765e9ad84081f5eb0ba448f39d1

endpoint: Limit proxy completion timeout to proxy updates
Wait for proxy redirect completion before generating policies that
redirect traffic to proxy redirect ports, to prevent traffic loss.
Reduce the proxy completion timeout to 10 seconds again, since
policy regeneration is now excluded from the timeout.

Fixes: #2788

Signed-off-by: Romain Lenglet <romain@covalent.io>

pkg/endpoint/policy.go
@@ -15,7 +15,6 @@
 package endpoint
 
 import (
-	"context"
 	"fmt"
 	"net"
 	"os"
@@ -26,7 +25,6 @@ import (
 
 	"github.com/cilium/cilium/api/v1/models"
 	"github.com/cilium/cilium/common"
-	"github.com/cilium/cilium/pkg/completion"
 	"github.com/cilium/cilium/pkg/controller"
 	"github.com/cilium/cilium/pkg/k8s"
 	"github.com/cilium/cilium/pkg/labels"
@@ -731,18 +729,11 @@ func (e *Endpoint) regenerate(owner Owner, reason string) (retErr error) {
 	}()
 
 	e.BuildMutex.Lock()
-	completionCtx, cancel := context.WithTimeout(context.Background(), 1*time.Minute)
-	e.ProxyWaitGroup = completion.NewWaitGroup(completionCtx)
-	defer func() {
-		cancel()
-		e.ProxyWaitGroup = nil
-		e.BuildMutex.Unlock()
-	}()
+	defer e.BuildMutex.Unlock()
 
-	e.Mutex.Lock()
+	e.Mutex.RLock()
 	e.getLogger().Debug("Regenerating endpoint...")
-	e.removeCollectedRedirects(owner)
-	e.Mutex.Unlock()
+	e.Mutex.RUnlock()
 
 	origDir := filepath.Join(owner.GetStateDir(), e.StringID())
 
@@ -767,10 +758,6 @@ func (e *Endpoint) regenerate(owner Owner, reason string) (retErr error) {
 	}()
 
 	revision, err := e.regenerateBPF(owner, tmpDir, reason)
-	if err == nil {
-		// Wait for all asynchronous proxy updates to be finished.
-		err = e.WaitForProxyCompletions()
-	}
 	if err != nil {
 		os.RemoveAll(tmpDir)
 		return err

daemon/endpoint.go
@@ -317,9 +317,6 @@ func (d *Daemon) deleteEndpoint(ep *endpoint.Endpoint) int {
 
 	// Wait for existing builds to complete and prevent further builds
 	ep.BuildMutex.Lock()
-	completionCtx, cancel := context.WithTimeout(context.Background(), 1*time.Minute)
-	defer cancel()
-	ep.ProxyWaitGroup = completion.NewWaitGroup(completionCtx)
 
 	// Lock out any other writers to the endpoint
 	ep.Mutex.Lock()
@@ -390,6 +387,9 @@ func (d *Daemon) deleteEndpoint(ep *endpoint.Endpoint) int {
 		errors++
 	}
 
+	completionCtx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	ep.ProxyWaitGroup = completion.NewWaitGroup(completionCtx)
+
 	errors += ep.LeaveLocked(d)
 	ep.Mutex.Unlock()
 
@@ -397,7 +397,9 @@ func (d *Daemon) deleteEndpoint(ep *endpoint.Endpoint) int {
 	if err != nil {
 		scopedLog.WithError(err).Warn("Error removing proxy redirects.")
 	}
+	cancel()
 	ep.ProxyWaitGroup = nil
+
 	ep.BuildMutex.Unlock()
 
 	return errors

pkg/endpoint/bpf.go
@@ -32,6 +32,7 @@ import (
 
 	"github.com/cilium/cilium/common"
 	"github.com/cilium/cilium/pkg/byteorder"
+	"github.com/cilium/cilium/pkg/completion"
 	"github.com/cilium/cilium/pkg/geneve"
 	"github.com/cilium/cilium/pkg/logging/logfields"
 	"github.com/cilium/cilium/pkg/maps/cidrmap"
@@ -440,6 +441,15 @@ func (e *Endpoint) regenerateBPF(owner Owner, epdir, reason string) (uint64, err
 
 	e.Mutex.Lock()
 
+	completionCtx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	e.ProxyWaitGroup = completion.NewWaitGroup(completionCtx)
+	defer func() {
+		cancel()
+		e.ProxyWaitGroup = nil
+	}()
+
+	e.removeCollectedRedirects(owner)
+
 	// If endpoint was marked as disconnected then
 	// it won't be regenerated.
 	// When building the initial drop policy in waiting-for-identity state
@@ -671,6 +681,14 @@ func (e *Endpoint) regenerateBPF(owner Owner, epdir, reason string) (uint64, err
 	libdir := owner.GetBpfDir()
 	rundir := owner.GetStateDir()
 	debug := strconv.FormatBool(owner.DebugEnabled())
+
+	// To avoid traffic loss, wait for the proxy to be ready to accept traffic
+	// on redirect ports, before we generate the policy that will redirect
+	// traffic to those ports.
+	err = e.WaitForProxyCompletions()
+	if err != nil {
+		return 0, fmt.Errorf("Error while configuring proxy redirects: %s", err)
+	}
 
 	err = e.runInit(libdir, rundir, epdir, epInfoCache.ifName, debug)
 	// CT entry clean up should always happen

commit 6c5133edb7971f2c2c96093e95bb8f175e0e4ad1

pkg/endpoint: fix endpoint.logger race condition
As getLogger specifies the endpoint.Mutex needs to be held,
endpoint.getLogger() after a mutex.Lock()

Fixes: 6a8b48951470 ("endpoint: Limit proxy completion timeout to proxy updates")
Signed-off-by: André Martins <andre@cilium.io>

pkg/endpoint/policy.go
@@ -689,9 +689,9 @@ func (e *Endpoint) regenerate(owner Owner, reason string) (retErr error) {
 	e.BuildMutex.Lock()
 	defer e.BuildMutex.Unlock()
 
-	e.Mutex.RLock()
+	e.Mutex.Lock()
 	e.getLogger().Debug("Regenerating endpoint...")
-	e.Mutex.RUnlock()
+	e.Mutex.Unlock()
 
 	origDir := filepath.Join(owner.GetStateDir(), e.StringID())
 